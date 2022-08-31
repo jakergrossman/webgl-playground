f2a9cf1797aee65cbd83477eb6a6994adadae5a0
jakergrossman <jake.r.grossman@gmail.com>
2022-08-31 15:10:51 -0500

    Add thin Program object wrapper around basic TWGL functions
    inspired heavily by https://github.com/skeeto/perlin-noise/blob/9f4957074d82385214311e8ce9caf2a70932c4b5/webgl/src/Program.js

--- perlin/main.js
+++ perlin/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 import * as twgl from "../dep/twgl.js/dist/4.x/twgl-full.module.js";
-twgl.setDefaults({attribPrefix: ""});
 import * as jake from "../lib/jake.module.js";
 
 const gl = document.getElementById("screen").getContext("webgl2");
@@ -9,29 +8,29 @@ if (!gl) {
     throw new Error("WebGL2 is not supported");
 }
 
+// initialize WebGL2 program
 const shaders = [ "shaders/identity.vert", "shaders/perlin.frag" ];
-await jake.webgl.generateShaderScriptTags(...shaders);
-
-const programInfo = twgl.createProgramInfo(gl, shaders);
+const program = new jake.webgl.Program(gl, ...shaders);
 
 const arrays = {
-    position: twgl.primitives.createAugmentedTypedArray(2, 4),
+    position: [
+        1.0,  1.0,
+       -1.0,  1.0,
+        1.0, -1.0,
+       -1.0, -1.0,
+    ],
 };
 
-arrays.position.push(1.0, 1.0);
-arrays.position.push(-1.0, 1.0);
-arrays.position.push(1.0, -1.0);
-arrays.position.push(-1.0, -1.0);
-
-const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+program
+    .arrays(arrays, 2)
+    .uniforms({
+        scale: 30.0,
+        zDepth: 0.0,
+        seed: jake.rand(0, 100),
+        rgb: 0,
+    }, false);
 
 let zSpeed = 0.03;
-const uniforms = {
-    scale: 30.0,
-    zDepth: 0.0,
-    seed: jake.rand(0, 100),
-    rgb: 0,
-};
 
 // initialize controls
 const controlPane  = document.getElementById("controls");
@@ -41,22 +40,24 @@ const seedControl = controlPane.elements[2];
 const rgbControl = controlPane.elements[3];
 
 // initial values
-scaleControl.value  = uniforms.scale;
+scaleControl.value  = program.uniformInfo.scale;
 zSpeedControl.value = zSpeed;
-seedControl.value   = uniforms.seed;
-rgbControl.checked  = false;
+seedControl.value   = program.uniformInfo.seed;
+rgbControl.checked  = program.uniformInfo.rgb;
 
 controlPane.oninput = updateParameters;
 
 function updateParameters() {
-    uniforms.scale = parseFloat(scaleControl.value);
-    zSpeed         = parseFloat(zSpeedControl.value);
-    uniforms.seed  = parseFloat(seedControl.value);
-    uniforms.rgb   = rgbControl.checked;
+    zSpeed = parseFloat(zSpeedControl.value),
+    program.uniforms({
+        scale: parseFloat(scaleControl.value),
+        seed: parseFloat(seedControl.value),
+        rgb: rgbControl.checked,
+    }, false);
 }
 
 function draw() {
-    uniforms.zDepth += zSpeed;  
+    program.uniformInfo.zDepth += zSpeed;
 
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -64,14 +65,14 @@ function draw() {
     gl.clearColor(0.83, 0.83, 0.83, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.useProgram(programInfo.program);
-    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-    twgl.setUniforms(programInfo, uniforms);
-    twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP);
-
-    if (gl.getError() !== gl.NO_ERROR) {
-        throw new Error("WebGL2 Rendering Error");
-    }
+    program
+        .use()
+        .buffersAndAttributes()
+        .uniforms()
+        .draw(gl.TRIANGLE_STRIP)
+        .glError(err => {
+            throw new Error("WebGL2 Rendering Error: " + err);
+        });
 
     requestAnimationFrame(draw);
 }
